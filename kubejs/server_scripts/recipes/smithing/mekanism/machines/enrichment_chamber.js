9015200d5e164eb902194c7f82385b91ea7b5831
TrueJustNeon <NeonWasHere25@gmail.com>
2026-03-15 19:30:20 -0300

    Update enrichment_chamber.js

--- kubejs/server_scripts/recipes/smithing/mekanism/machines/enrichment_chamber.js
+++ kubejs/server_scripts/recipes/smithing/mekanism/machines/enrichment_chamber.js
@@ -1,7 +1,7 @@
 ServerEvents.recipes(event => {
 event.smithing(
   'mekanism:enrichment_chamber',                     // arg 1: output
-  'kubejs:plastic_mechanism', // arg 2: the smithing template
+  'create:precision_mechanism', // arg 2: the smithing template
   'kubejs:zinc_machine',                          // arg 3: the item to be upgraded
   'mekanism:basic_control_circuit'                            // arg 4: the upgrade item
 )
