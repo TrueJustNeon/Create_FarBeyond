a6a963081f57254c516cb774cc4751f6e59b6698
TrueJustNeon <NeonWasHere25@gmail.com>
2026-03-15 23:50:24 -0300

    Recipe fix
    Recipe fixed for:
    Contraption Controls(Create)
    Potion Diffuser Device(Thermal)

--- kubejs/server_scripts/recipes/cutting/create/andesite_machine/contraption_controls.js
+++ kubejs/server_scripts/recipes/cutting/create/andesite_machine/contraption_controls.js
@@ -1,6 +1,6 @@
 ServerEvents.recipes(event => {
     event.stonecutting(
-        Item.of('create:contraption_controller', 1), // Output??
+        Item.of('create:contraption_controls', 1), // Output??
         Item.of('kubejs:andesite_machine') // Input??
 
     )
